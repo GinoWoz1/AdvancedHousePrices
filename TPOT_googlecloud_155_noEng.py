7969ffed377167372ea28bdbcab58a79cec7ebee
GinoWoz1 <jstnjc@gmail.com>
2018-09-15 18:47:35 -0700

    changes for 155 column run

--- TPOT_googlecloud_155_noEng.py
+++ TPOT_googlecloud_155_noEng.py
@@ -11,6 +11,7 @@ import warnings
 import pandas as pd
 import math
 import os
+import numpy as np
 
 mingw_path = 'C:\\Users\\jstnjc\\Anaconda3\\Library\\mingw-w64\\bin'
 os.environ['PATH'] = mingw_path + ';' + os.environ['PATH']
@@ -20,8 +21,11 @@ warnings.filterwarnings('ignore')
 url = 'https://github.com/GinoWoz1/AdvancedHousePrices/raw/master/'
 
 X_train = pd.read_csv(url + 'x_train_155_noEng.csv')
+X_train.drop(['Unnamed: 0'],axis=1,inplace=True)
 
 y_train = pd.read_csv(url + 'y_train.csv', header=None)
+y_train.rename(columns={0:'SalePrice'},inplace=True)
+y_train['SalePrice'] = y_train['SalePrice'].astype(float)
 
 def rmsle_loss(y_true, y_pred):
     assert len(y_true) == len(y_pred)
@@ -35,5 +39,5 @@ def rmsle_loss(y_true, y_pred):
 
 rmsle_loss = make_scorer(rmsle_loss,greater_is_better=False)
 
-tpot = TPOTRegressor(random_state=1,verbosity=3, scoring = rmsle_loss,periodic_checkpoint_folder='C:\\Users\\jstnjc\\Google Drive\\Kaggle\\Advanced House Prices\\Eng200', population_size=300,offspring_size= 400,max_eval_time_mins=10, use_dask=True)
-tpot.fit(X_train,y_train[0])
+tpot = TPOTRegressor(random_state=1,verbosity=3, scoring = rmsle_loss,periodic_checkpoint_folder='C:\\Users\\jstnjc\\Google Drive\\Kaggle\\Advanced House Prices\\NoEng200', population_size=300,offspring_size= 400,max_eval_time_mins=10, use_dask=True)
+tpot.fit(np.array(X_train),np.array(y_train))
